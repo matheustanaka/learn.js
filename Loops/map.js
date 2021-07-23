07f6c3dd01a8d2e32c4f5697308ef747989ff10e
Matheus_Tanaka <matheustanakanog@gmail.com>
2021-07-22 22:38:12 -0300

    Practicing map()

--- Loops/map.js
+++ Loops/map.js
@@ -28,3 +28,40 @@ _.map =
     return val + 1;
   });
 //Output: [2,3,4]
+
+// Practicing
+function multiplicaPor2(x) {
+  return x * 2;
+}
+
+function dividepor3(x) {
+  return x / 3;
+}
+
+let arr = [15, 12, 3, 24, 21];
+
+// console.log('Array Original')
+// console.log(arr)
+// console.log('Resultado usando a função Map')
+// arr.map(dividepor3);
+
+// console.log('Array Original')
+// console.log(arr)
+// console.log('Resultado usando a função Map')
+// arr.map(multiplicaPor2);
+
+// Fazendo de outra forma
+
+let strings = ["matheusin", "felipin", "bielin"];
+
+console.log("Array original");
+console.log(strings);
+console.log("Novo Array com os nomes em maiusculo");
+
+//Primeira forma
+strings.map((n) => n.toUpperCase());
+
+//Segunda forma, ambas as funções fazem a mesma coisa
+strings.map(function toUpperCase(n) {
+  return n.toUpperCase();
+});
