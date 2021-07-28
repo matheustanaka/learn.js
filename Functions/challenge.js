e84ec4205fc9704fb7e8ba1dbd843e5d64c557f0
Matheus_Tanaka <matheustanakanog@gmail.com>
2021-07-28 14:10:57 -0300

    Finishing challenge

---
+++ Functions/challenge.js
@@ -0,0 +1,82 @@
+const videoData = [
+  {
+    name: "Miss Scarlet",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Mrs. White",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Reverend Green",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Rusty",
+    present: false,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Colonel Mustard",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+  {
+    name: "Professor Plum",
+    present: true,
+    rooms: [
+      { kitchen: false },
+      { ballroom: false },
+      { conservatory: false },
+      { "dining room": false },
+      { "billiard room": false },
+      { library: false },
+    ],
+  },
+];
+
+const suspects = videoData.filter((suspectsObject) => {
+  return suspectsObject.present;
+});
+
+const suspectsNames = videoData.map(suspects, (suspect) => {
+  return suspect.name;
+});
