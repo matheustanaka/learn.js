0b8950e6afe2418076ae05306fef716c1d8df1fe
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-12 18:16:27 -0300

    feat: add new solution for arrayChange

---
+++ Exercises/arrayChange.js
@@ -0,0 +1,11 @@
+
+function arrayChange(array){
+    // Receber um array de numeros e retornar um array com os mesmos numeros, mas como strings.
+    array = [1,2,3,4,5]
+    //Usando a função toString
+    console.log("Em Strings: " + array.toString());
+    
+  }
+  //Executando a função
+  arrayChange();
+  //OUTPUT: Em Strings: 1,2,3,4,5
