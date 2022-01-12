26d760a85e7325c95619e884b732e92ef6cb4319
Matheus Tanaka <matheustanakanog@gmail.com>
2022-01-12 17:35:29 -0300

    feat: add new solution for array position

---
+++ Exercises/arrayPosition.js
@@ -0,0 +1,12 @@
+function arrayPosition(arr){
+    // Retornar o quinto item do array recebido na function.
+   var arr = ["lucas", "pedro", "joão", "matheus", "tobias", "josias", "marcelo"];
+   // "var i" vai percorrer o array, e quando ele chegar na posição 5, vai retornar o elemento da posição 5
+     for(var i = 0; i < arr.length; i++) {
+       if(arr[i] = arr[i = 5]){
+         return arr[i = 5];
+       }
+     }
+   }
+   //executando a função
+   arrayPosition();
