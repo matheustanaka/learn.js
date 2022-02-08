2b83e0547a5a8ce4c8fca98541285483676eed72
Matheus Tanaka <matheustanakanog@gmail.com>
2022-02-08 16:47:00 -0300

    feat: add new solution

--- MC102/comandos-condicionais/ex1.js
+++ MC102/comandos-condicionais/ex1.js
@@ -1,15 +1,14 @@
 /*Escreva um programa que, dados três números inteiros,
 imprima o menor deles.
  */
-
 const number1 = Number(prompt("digite um número:"));
 const number2 = Number(prompt("digite um número:"));
 const number3 = Number(prompt("digite um número:"));
 
 if (number1 < number2 && number1 < number3) {
   console.log(number1);
-} else if (number2 < number1 && number2 < number3) {
+} else if (number2 < number3) {
   console.log(number2);
-} else if (number3 < number1 && number3 < number2) {
+} else {
   console.log(number3);
 }
